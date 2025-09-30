4439fc2985ae67344ea66b5584f4de111306ad2f
luzdelaluna <annalis090@gmail.com>
2025-10-01 01:23:51 +0300

    add cors

--- app/main.py
+++ app/main.py
@@ -22,7 +22,10 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:3000",
+    "http://localhost:3001",
     "http://localhost:8000",
+    'http://admin.tatroof.ru',
+    'http://tatroof.ru',
     'http://api.tatroof.ru'
 ]
 
